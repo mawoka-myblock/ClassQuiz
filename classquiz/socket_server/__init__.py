4f668748e12f210afaa365afa78bc29a5b5b4bd4
Mawoka <mawoka@e.email>
2022-02-28 15:07:45 +0100

    Made get_question_results "public"

--- classquiz/socket_server/__init__.py
+++ classquiz/socket_server/__init__.py
@@ -77,10 +77,9 @@ async def register_as_admin(sid, data):
 @sio.event
 async def get_question_results(sid, data):
     session = await sio.get_session(sid)
-    if session["admin"]:
-        redis_res = await redis.get(f"game_session:{session['game_pin']}:{data['question_number']}")
-        game_pin = session['game_pin']
-        await sio.emit("question_results", redis_res, room=game_pin)
+    redis_res = await redis.get(f"game_session:{session['game_pin']}:{data['question_number']}")
+    game_pin = session['game_pin']
+    await sio.emit("question_results", redis_res, room=game_pin)
 
 
 @sio.event
