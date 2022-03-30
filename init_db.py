ce2a5dd7a834ff6a92fe21065a620d1e7ba6c332
Mawoka <mawoka@e.email>
2022-03-30 18:36:00 +0200

    :ambulance: Fixed init_db.py

--- init_db.py
+++ init_db.py
@@ -2,7 +2,7 @@ from classquiz.config import settings
 import sqlalchemy
 from classquiz.db import metadata
 
-engine = sqlalchemy.create_engine(settings.db_url)
+engine = sqlalchemy.create_engine(settings().db_url)
 
 metadata.create_all(engine)
 print("Database setup finished!")
