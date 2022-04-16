1a52951f3404c265237e6854d76773c53158e99d
Mawoka <mawoka@e.email>
2022-04-16 11:33:31 +0200

    :art: Formatted code

--- frontend/src/routes/account/register.svelte
+++ frontend/src/routes/account/register.svelte
@@ -352,7 +352,7 @@
 					type="button"
 					on:click={() => {
 						responseData.open = false;
-						window.location.replace("/");
+						window.location.replace('/');
 					}}
 					class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
 					>{$t('words.close')}
