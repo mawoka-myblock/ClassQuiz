d756e379a029a4749daeca42851b46f2d3ba0745
Mawoka <mawoka-myblock@e.email>
2022-04-18 16:18:40 +0200

    :memo: Updated privacy-policy.svelte

--- frontend/src/routes/docs/privacy-policy.svelte
+++ frontend/src/routes/docs/privacy-policy.svelte
@@ -31,27 +31,24 @@
 		</thead>
 		<tbody>
 			<tr>
-				<td> Your IP-Address</td>
+				<td> The IP-Address of the user</td>
 				<td>
 					<ul>
-						<li>
-							I store them because I am too lazy to configure them to be not stored.
-						</li>
 						<li>To prevent abuse</li>
 						<li>To provide user-sessions</li>
 					</ul>
 				</td>
 				<td> For a very long time</td>
-				<td> On my server, by Netcup, in Germany.</td>
+				<td> On the developers server, by Netcup, in Germany.</td>
 			</tr>
 			<tr>
 				<td> The data the user enters (quizzes, email, username)</td>
 				<td> Self-explaining</td>
 				<td> Maximum of 30 days after you've deleted your account</td>
-				<td> On my server, by Netcup, in Germany.</td>
+				<td> On the developers server, by Netcup, in Germany.</td>
 			</tr>
 			<tr>
-				<td>The hashed password of the user</td>
+				<td>The <b>hashed</b> password of the user</td>
 				<td>Self-explaining</td>
 				<td>Maximum of 30 days after you've deleted your account</td>
 				<td>On my server, by Netcup, in Germany.</td>
