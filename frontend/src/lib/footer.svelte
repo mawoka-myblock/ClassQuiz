a07e3626b22b45b5ae78736b4028822560f42f11
Mawoka <mawoka@e.email>
2022-04-26 18:32:30 +0200

    :busts_in_silhouette: Fixed #35

--- frontend/src/lib/footer.svelte
+++ frontend/src/lib/footer.svelte
@@ -6,6 +6,10 @@
 			class="hover:underline bg-clip-text text-transparent bg-gradient-to-r from-[#009444] via-[#39b54a] to-[#8dc63f]"
 			>Mawoka</a
 		>
+		and with the help of
+		<a href="/docs/attribution" class="underline text-blue-300 hover:text-blue-500 transition"
+			>others</a
+		>.
 	</div>
 	<div class="flex justify-center bg-gray-700 border-t-2 border-t-white">
 		<p class="text-gray-400 text-center">
