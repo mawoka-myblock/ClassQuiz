4075cf086c0f1011cba37c8f27fe318570eccbff
Mawoka <mawoka@e.email>
2022-05-09 17:00:19 +0200

    :art: Formatted code

--- frontend/src/lib/footer.svelte
+++ frontend/src/lib/footer.svelte
@@ -1,25 +1,34 @@
-<footer class='text-center text-white border-black'>
-	<div class='text-center pt-4 bg-gray-700'>
+<footer class="text-center text-white border-black">
+	<div class="text-center pt-4 bg-gray-700">
 		Made with ❤️ by
 		<a
-			href='https://mawoka.eu?utm_source=ClassQuiz&utm_medium=footer'
-			target='_blank'
-			class='hover:underline bg-clip-text text-transparent bg-gradient-to-r from-[#009444] via-[#39b54a] to-[#8dc63f]'
-		>Mawoka</a
+			href="https://mawoka.eu?utm_source=ClassQuiz&utm_medium=footer"
+			target="_blank"
+			class="hover:underline bg-clip-text text-transparent bg-gradient-to-r from-[#009444] via-[#39b54a] to-[#8dc63f]"
+			>Mawoka</a
 		>
 		and with the help of
-		<a href='/docs/attribution' class='underline text-blue-300 hover:text-blue-500 transition'
-		>others</a
+		<a href="/docs/attribution" class="underline text-blue-300 hover:text-blue-500 transition"
+			>others</a
 		>.
 	</div>
-	<div class='text-center bg-gray-700 pb-2'>
-		<p>If you find this useful, please consider <a href='https://mawoka.eu/donate' target='_blank'
-													   class='underline text-blue-300 hover:text-blue-500 transition'>Donating</a>.
-			<a href='https://mawoka.eu/donate' target='_blank'
-			   class='underline text-blue-300 hover:text-blue-500 transition'>More details are here.</a></p>
+	<div class="text-center bg-gray-700 pb-2">
+		<p>
+			If you find this useful, please consider <a
+				href="https://mawoka.eu/donate"
+				target="_blank"
+				class="underline text-blue-300 hover:text-blue-500 transition">Donating</a
+			>.
+			<a
+				href="https://mawoka.eu/donate"
+				target="_blank"
+				class="underline text-blue-300 hover:text-blue-500 transition"
+				>More details are here.</a
+			>
+		</p>
 	</div>
-	<div class='flex justify-center bg-gray-700 border-t-2 border-t-white'>
-		<p class='text-gray-400 text-center'>
+	<div class="flex justify-center bg-gray-700 border-t-2 border-t-white">
+		<p class="text-gray-400 text-center">
 			<i>Kahoot! and the K! logo are trademarks of Kahoot! AS</i>
 		</p>
 	</div>
