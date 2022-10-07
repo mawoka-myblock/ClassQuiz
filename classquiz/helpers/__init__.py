f5d49af44fb0262cebc5a00d92f9836efbe326b2
Mawoka <git@mawoka.eu>
2022-10-07 18:52:37 +0200

    :goal_net: Catching error which could occur on export

--- classquiz/helpers/__init__.py
+++ classquiz/helpers/__init__.py
@@ -56,7 +56,10 @@ async def generate_spreadsheet(quiz_results: dict, quiz: Quiz, player_fields: di
     for i, _ in enumerate(quiz_results):
         question = quiz.questions[i]
         print(quiz_results)
-        answer_data = quiz_results[str(i)]
+        try:
+            answer_data = quiz_results[str(i)]
+        except KeyError:
+            continue
         worksheet.write(i + 1, 0, question["question"])
         worksheet.write(i + 1, 1, question["time"])
 
