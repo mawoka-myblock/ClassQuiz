7dbfdbcd39392db3fe362d695d370469a9aecee0
Mawoka <git@mawoka.eu>
2023-10-04 13:04:04 +0200

    Added note about player limit

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -79,6 +79,10 @@ SPDX-License-Identifier: MPL-2.0
 
 	const classquiz_reasons = [
 		{
+			headline: $t('index_page.no_player_limit'),
+			content: $t('index_page.no_player_limit_content'),
+		},
+		{
 			headline: $t('index_page.no_tracking'),
 			content: $t('index_page.no_tracking_content')
 		},
