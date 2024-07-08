0acd34aad5e23895a5a2f246496d616f46ad63af
Mawoka <git@mawoka.eu>
2024-07-08 11:32:59 +0200

    Cleanup of run_tests.sh script

--- frontend/src/lib/components/commandpalette.svelte
+++ frontend/src/lib/components/commandpalette.svelte
@@ -247,7 +247,6 @@ This should be okay, right?
 				</p>
 				<input
 					type="text"
-                    comment="svelte recommends against autofocus for accessibility reasons. Therefore removed autofocus command for this input"
 					class="col-start-1 row-start-1 bg-transparent w-full p-4 outline-none bg-gray-700 rounded"
 					bind:value={input}
 				/>
--- run_tests.sh
+++ run_tests.sh
@@ -9,18 +9,13 @@ run_tests() {
 }
 
 stop() {
-  $CONTAINER_BIN container stop classquiz_db
-  $CONTAINER_BIN container stop test_redis
-  $CONTAINER_BIN container stop test_meili
+  $CONTAINER_BIN compose -f docker-compose.dev.yml down --volumes
 }
 
 init() {
   mkdir /tmp/storage
-  $CONTAINER_BIN run --rm -d -p 6379:6379 --name test_redis redis:alpine
-  $CONTAINER_BIN run -it --rm -d -p 7700:7700 --name test_meili getmeili/meilisearch:v0.28.0
-  $CONTAINER_BIN volume create classquiz_db_data
-  $CONTAINER_BIN run --name classquiz_db -p 5432:5432 --rm -d -e POSTGRES_PASSWORD=mysecretpassword -v classquiz_db_data:/var/lib/postgresql/data -e POSTGRES_DB=classquiz postgres
-  sleep 1
+  $CONTAINER_BIN compose -f docker-compose.dev.yml up -d
+  sleep 2
   pipenv run alembic upgrade head
 }
 
@@ -28,14 +23,14 @@ case $1 in
 +) init ;;
 -) stop ;;
 a)
-  $CONTAINER_BIN volume rm classquiz_db_data
+  $CONTAINER_BIN volume rm classquiz_db
   init
   run_tests
   stop
   ;;
 prepare)
   stop
-  $CONTAINER_BIN volume rm classquiz_db_data
+  $CONTAINER_BIN volume rm cclassquiz_db
   init
   ;;
 *)
