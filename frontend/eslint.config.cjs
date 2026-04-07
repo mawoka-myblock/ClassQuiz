4b428f79325bd6554969a5e8ca23d0d3d0efecf1
Proxyfil <piloupilou200@gmail.com>
2026-04-07 16:39:49 +0200

    📄 Added licences for the 2 new files

--- frontend/eslint.config.cjs
+++ frontend/eslint.config.cjs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 Marlon W (Mawoka)
+//
+// SPDX-License-Identifier: MPL-2.0
+
 const { defineConfig, globalIgnores } = require('eslint/config');
 
 const tsParser = require('@typescript-eslint/parser');
