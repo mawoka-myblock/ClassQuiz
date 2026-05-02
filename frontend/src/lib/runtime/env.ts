c379ad0def334fb0d79d919721bd5812b0b65210
Proxyfil <piloupilou200@gmail.com>
2026-05-02 21:50:02 +0200

    ⚡️ Reformated code to make it easier to understand and use

--- frontend/src/lib/runtime/env.ts
+++ frontend/src/lib/runtime/env.ts
@@ -2,12 +2,23 @@
 //
 // SPDX-License-Identifier: MPL-2.0
 
+/**
+ * This wrapper allow to both, frontend and backend, to get env vars that are set at runtime in the Dockerfile.
+ * It will first try to get the env var from the window object, if it fails (which means we are in the backend), it will try to get it from process.env.
+ */
 export const runtimeEnvVarWrapper = {
-	get: function (name) {
-		try {
-			return window.env[name];
-		} catch (e) {
-			return process.env[name];
-		}
-	}
+
+    /**
+     * This function allow to get env vars that are set at runtime in the Dockerfile
+     * @param name Name of the env var we want
+     * @returns Value of the env var
+     */
+    get(name: string): string | undefined {
+        try {
+            return window.env[name];
+        }
+        catch (_) {
+            return process.env[name];
+        }
+    }
 };
--- frontend/static/scripts/env.template.js
+++ frontend/static/scripts/env.template.js
@@ -2,8 +2,11 @@
 //
 // SPDX-License-Identifier: MPL-2.0
 
+// This function is called in frontend
+// Each variable is replaced by envsubst in the Dockerfile, so that they can be set at runtime
+// The syntax should be ```window.env.VARIABLE_NAME = '${VARIABLE_NAME}';```
 (function (window) {
 	window.env = window.env || {};
 
-	window['env']['TEST_VARIABLE'] = '${TEST_VARIABLE}';
+  window.env.TEST_VARIABLE = '${TEST_VARIABLE}';
 })(this);
